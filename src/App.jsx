b4fa0ebf51cbffb0d593847d509898b6857fe630
gabriel-lavilledevaure <gabriel.lavilledevaure@etu.unilim.fr>
2026-09-04 11:21:15 +0200

    Update App.JSX

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,12 @@
-const name = "Hedy Lamarr";
-const age = 30;
-const monStyle = { backgroundColor: "lightgray", padding: "20px" };
-
 function App() {
-  const hello = (nom) => {
-    return nom;
-  };
   return (
     <>
-      <h1 className="card__title">{name}'s Todos</h1>
-      <img className="card__img" src="https://i.imgur.com/yXOvdOSs.jpg"></img>
-      <ul style={monStyle}>
-        <li>Invent new traffic lights {age + 1}</li>
-        <li className="list__item">Rehearse a movie scene {hello("Bill")}</li>
-        <li className="list__item">Improve the spectrum technology</li>
+      <h1 className="bg-amber-300">Hedy Lamarr's Todos</h1>
+      <img src="https://i.imgur.com/yXOvdOSs.jpg" alt="Hedy Lamarr" />
+      <ul>
+        <li>Invent new traffic lights</li>
+        <li>Rehearse a movie scene</li>
+        <li>Improve the spectrum technology</li>
       </ul>
     </>
   );
